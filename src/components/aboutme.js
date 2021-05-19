9bef0c77fa44bfc60189ff8cff22014d4bd7f415
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-19 16:56:41 -0400

    about responsive text

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -7,7 +7,7 @@ class About extends Component {
       <div className="aboutpage">
         <div className='col1'>
           <h2>About Me</h2>
-          <p>
+          <p className='abouttext'>
             My name is Isaiah Silvani. I believe that in life, if you're going to do anything, you might as well do it the best you can. What I love most about software development is that since no one can ever take your knowledge away, your development skills will only improve over time with hard work. I strive to always push myself and produce amazing results when a task is at hand. When I'm not englufed in a coding hole, you can find my playing games on my Playstation or going on walks at night. 
           </p>
         </div>
