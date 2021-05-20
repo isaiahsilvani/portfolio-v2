ac365d8d2418dd680a419692f7b78b0dd6890e40
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-19 20:20:04 -0400

    one more typo

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -8,7 +8,7 @@ class About extends Component {
         <div className='col1'>
           <h2>About Me</h2>
           <p className='abouttext'>
-            My name is Isaiah Silvani. I believe that in life, if you're going to do anything, you might as well do it the best you can. What I love most about software development is that since no one can ever take your knowledge away, your development skills will only improve over time with hard work. I strive to always push myself and produce amazing results when a task is at hand. When I'm not englufed in a coding hole, you can find myeplaying games on my Playstation or going on walks at night. 
+            My name is Isaiah Silvani. I believe that in life, if you're going to do anything, you might as well do it the best you can. What I love most about software development is that since no one can ever take your knowledge away, your development skills will only improve over time with hard work. I strive to always push myself and produce amazing results when a task is at hand. When I'm not indulging in a coding hole, you can find my playing games on my Playstation, hanging out with friends and family or going on a late night walk. 
           </p>
         </div>
         <div className='col2'>
